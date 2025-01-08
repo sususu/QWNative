536131bcf12a2283cfca22101e49caf9f10494c9
sunima <sujiangbest@163.com>
2025-01-08 21:01:48 +0800

    Add files via upload

---
+++ QWNative.framework/Headers/QWNative.h
@@ -0,0 +1,18 @@
+//
+//  QWNative.h
+//  QWNative
+//
+//  Created by apple on 2024/12/31.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QWNative.
+FOUNDATION_EXPORT double QWNativeVersionNumber;
+
+//! Project version string for QWNative.
+FOUNDATION_EXPORT const unsigned char QWNativeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QWNative/PublicHeader.h>
+
+
